a19617c47f07789cca3a8f69e9af6a665a3c7f62
Angri45 <ssni4655@gmail.com>
2025-02-18 12:08:28 +0200

    V0.1

---
+++ index.js
@@ -0,0 +1,10 @@
+var salary = 55;
+var userData = '{ "isBirtdayData": true ,"ageData":40,"userNameData":"John"}';
+var arr = ['sss', 2, 2];
+var userObj = JSON.parse(userData);
+console.log(userObj);
+var isOkay = true;
+var movment = false;
+if (isOkay) {
+    movment = 'moving';
+}
