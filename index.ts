97f0fa4539b8584a2bfe71a0de1cda01dbb6e6d0
Angri45 <ssni4655@gmail.com>
2025-02-18 12:03:20 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    
    Initial commit
    
    Changes to be committed:
    	new file:   index.ts
    	new file:   tsconfig.json

---
+++ index.ts
@@ -0,0 +1,21 @@
+const salary = 55;
+
+interface UserDate {
+  isBirtdayData: boolean;
+  ageData: number;
+  userNameData: string;
+}
+
+const userData = '{ "isBirtdayData": true ,"ageData":40,"userNameData":"John"}';
+
+const arr = ['sss', 2, 2];
+const userObj: UserDate = JSON.parse(userData);
+
+console.log(userObj);
+
+const isOkay = true;
+let movment: boolean | string = false;
+
+if (isOkay) {
+  movment = 'moving';
+}
